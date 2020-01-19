8a641367b4f33787fbcf1ee01f9652a3190a5dd0
jinghui.zhang <jinghui.zhang@thoughtworks.com>
2020-01-19 19:34:51 +0800

    add a alpine image with bash

--- Dockerfiles/alpine/build.sh
+++ Dockerfiles/alpine/build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker built -t 5200710/alpine:3.11.3 .
+cur_dir=$(pwd)
+# login in shell folder
+cd $(dirname "$0")
+# SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+# echo $SHELL_FOLDER
+
+docker build -t 5200710/alpine:3.11.3 .
+
+# go back
+cd ${cur_dir}
