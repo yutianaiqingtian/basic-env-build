9663f69db509895d089ac5b90970ec34343c5f1f
jinghui.zhang <jinghui.zhang@thoughtworks.com>
2020-03-12 00:59:46 +0800

    refactor the dockerfile code

--- hadoop/pseudo-distributed/entrypoint.sh
+++ hadoop/pseudo-distributed/entrypoint.sh
@@ -9,8 +9,8 @@ su - hadoop -c "${HADOOP_HOME}/sbin/start-dfs.sh"
 su - hadoop -c "${HADOOP_HOME}/sbin/start-yarn.sh"
 
 if [[ -z "$1" ]]; then
-  # while true; do sleep 1000; done
-  tail -f "${HADOOP_HOME}/logs/*.log"
+  while true; do sleep 1000; done
+  # tail -f "${HADOOP_HOME}/logs/*.log"
 fi
 
 if [[ "bash" == "$1" ]]; then
