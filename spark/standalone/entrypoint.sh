74afad84893405d3bb9e6105ad156c0c109d1161
jinghui.zhang <15671678163@163.com>
2020-05-13 22:37:25 +0800

    update a spark path

--- spark/standalone/entrypoint.sh
+++ spark/standalone/entrypoint.sh
@@ -3,6 +3,9 @@
 # Exit immediately if a command exits with a non-zero exit status.
 set -e
 
+[ -z $JAVA_HOME ] && JAVA_HOME='/root/.sdkman/candidates/java/current'
+[ -z $SPARK_HOME ] && SPARK_HOME='/root/.sdkman/candidates/spark/current'
+
 if [ ! -e ~/.ssh/authorized_keys ]; then
     /usr/bin/ssh-keygen -t rsa -P "" -f ~/.ssh/id_rsa
     cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
@@ -12,13 +15,13 @@ fi
 service ssh start
 
 # 指定一个Slave和Java变量
-echo "localhost" > /opt/spark/conf/slaves
-echo "JAVA_HOME=/root/.sdkman/candidates/java/current" > /opt/spark/conf/spark-env.sh
+echo "localhost" > ${SPARK_HOME}/conf/slaves
+echo "JAVA_HOME=${JAVA_HOME}" > ${SPARK_HOME}/conf/spark-env.sh
 
-/opt/spark/sbin/start-all.sh
+${SPARK_HOME}/sbin/start-all.sh
 
 if [[ $1 == "-d" ]]; then
-    tail -f /opt/spark/logs/*.out
+    tail -f ${SPARK_HOME}/logs/*.out
     # while true; do sleep 1000; done
 fi
 
