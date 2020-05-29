c1a3b3f55a54de84fd631f877bb96c4511afc989
jinghui.zhang <15671678163@163.com>
2020-05-29 13:09:53 +0800

    add a python in spark standalone images

--- spark/standalone.dockerfile
+++ spark/standalone.dockerfile
@@ -1,8 +1,10 @@
 FROM 5200710/spark:2.4.4-local
 
-RUN softwares='openssh-server openssh-client' \
+RUN softwares='openssh-server openssh-client python3.7 python3-pip' \
     && apt-get update && apt-get install -y $softwares \
     && rm -rf /var/lib/apt/lists/* 
+
+ENV PYSPARK_PYTHON python3
 
 COPY ./standalone/entrypoint.sh /entrypoint.sh
 
--- spark/standalone/entrypoint.sh
+++ spark/standalone/entrypoint.sh
@@ -3,20 +3,25 @@
 # Exit immediately if a command exits with a non-zero exit status.
 set -e
 
+# Install custom python package if requirements.txt is present
+if [ -e "/requirements.txt" ]; then
+    pip3 install --user -r /requirements.txt
+fi
+
 [ -z $JAVA_HOME ] && JAVA_HOME='/root/.sdkman/candidates/java/current'
 [ -z $SPARK_HOME ] && SPARK_HOME='/root/.sdkman/candidates/spark/current'
 
 if [ ! -e ~/.ssh/authorized_keys ]; then
     /usr/bin/ssh-keygen -t rsa -P "" -f ~/.ssh/id_rsa
-    cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
+    cat ~/.ssh/id_rsa.pub >~/.ssh/authorized_keys
     chmod 0600 ~/.ssh/authorized_keys
 fi
 
 service ssh start
 
 # 指定一个Slave和Java变量
-echo "localhost" > ${SPARK_HOME}/conf/slaves
-echo "JAVA_HOME=${JAVA_HOME}" > ${SPARK_HOME}/conf/spark-env.sh
+echo "localhost" >${SPARK_HOME}/conf/slaves
+echo "JAVA_HOME=${JAVA_HOME}" >${SPARK_HOME}/conf/spark-env.sh
 
 ${SPARK_HOME}/sbin/start-all.sh
 
